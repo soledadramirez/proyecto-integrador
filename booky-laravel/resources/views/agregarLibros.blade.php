11301afe3e1f1bcfdef31b3150c5378ca6720eac
Federico <fedefri@hotmail.com>
2019-07-03 19:29:05 -0300

    arreglo-links

--- booky-laravel/resources/views/agregarLibros.blade.php
+++ booky-laravel/resources/views/agregarLibros.blade.php
@@ -4,7 +4,7 @@
 
 <link rel="stylesheet" type="text/css" href="css/agregarLibros.css">
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#"><h1>Booky</h1> </a>
+  <a class="navbar-brand" href="/home"><h1>Booky</h1> </a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -13,7 +13,7 @@
 
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="/home">Home<span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#">Nosotros</a>
--- booky-laravel/resources/views/bookPost.blade.php
+++ booky-laravel/resources/views/bookPost.blade.php
@@ -7,7 +7,7 @@
 
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#"><h1>Booky</h1> </a>
+  <a class="navbar-brand" href="/home"><h1>Booky</h1> </a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -16,7 +16,7 @@
 
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="/home">Home<span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#">Nosotros</a>
