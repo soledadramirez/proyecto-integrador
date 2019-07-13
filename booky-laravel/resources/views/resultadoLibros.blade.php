71364a99b5ae26991902b5a1eab359753639816a
Federico <fedefri@hotmail.com>
2019-07-13 16:45:27 -0300

    ver.mas

--- booky-laravel/resources/views/resultadoLibros.blade.php
+++ booky-laravel/resources/views/resultadoLibros.blade.php
@@ -18,16 +18,19 @@
               <li class="list-group-item">  Autor: {{$libro->author->name}} </li>
               <li class="list-group-item">Estado de este ejemplar: {{$libro->state->name}}</li>
 
+
+             @if (!AUTH::user())
               <a class="btn btn-success mt-3 myBtn2">Ver más</a>
 
 
               <div class="modal2 myModal">
 
 
-                <div class="modal-content2">
+                <div class="modal-content2" style="text-align:center">
 
 
-                  <h1> <span class="close2">&times;</span> Tenés que estar logueado!!!!</h1>
+                  <h1> <span class="close2">&times;</span> Para ver los detalles del libro debes estar logueado.</h1>
+                  <h3>Haz click <a style="color: blue" href="/login">aquí </a> si tienes una cuenta o <a style="color: blue" href="/register"> aquí</a>  para registrarte</h3>
 
 
               </div>
@@ -37,6 +40,16 @@
 
           </div>
         </div>
+
+          @else
+
+          <a href="bookPost/{{$libro->id}}"   class="btn btn-success mt-3 myBtn2">Ver más</a>
+         </ul>
+       </div>
+     </div>
+
+
+        @endif
       @empty
           <div class="altura minima">
             No hay libros disponibles
